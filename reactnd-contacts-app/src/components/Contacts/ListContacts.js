da384ba4360206a50fe190e37c18feb1b3cdef9b
marcio <marciocamello@outlook.com>
2019-01-17 11:58:02 -0200

    Finish lesson 3

--- reactnd-contacts-app/src/components/Contacts/ListContacts.js
+++ reactnd-contacts-app/src/components/Contacts/ListContacts.js
@@ -1,12 +1,28 @@
 import React, { Component } from 'react'
 
+import './ListContacts.css';
+
 class ListContacts extends Component {
     render() {
         return (
             <ol className='contact-list'>
-                {this.props.contacts.map(contact => {
-                    return <li key={contact.id}>{contact.name}</li>
-                })}
+                {this.props.contacts.map(contact => (
+                    <li key={contact.id} className='contact-list-item'>
+                        <div 
+                            className='contact-avatar'
+                            style={{
+                                backgroundImage: `url(${contact.avatarURL})`
+                            }}
+                        ></div>
+                        <div className='contact-details'>
+                            <p>{contact.name}</p>
+                            <p>{contact.handle}</p>
+                        </div>
+                        <button className='contact-remove'>
+                            Remove
+                        </button>
+                    </li>
+                ))}
             </ol>
         )
     }
