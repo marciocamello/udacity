d267cf434ea9f7bda04f8ae97af08e9ae4e89d27
marcio <marciocamello@outlook.com>
2019-01-18 12:01:08 -0200

    Finish lesson 12

--- reactnd-contacts-app/src/App.js
+++ reactnd-contacts-app/src/App.js
@@ -25,10 +25,21 @@ class App extends Component {
      ]
   }
 
+  removeContact = contact => {
+    this.setState(currentState => ({
+      contacts: currentState.contacts.filter(c => {
+        return c.id !== contact.id
+      })
+    }))
+  }
+
   render() {
     return (
       <div>
-        <ListContacts contacts={this.state.contacts} />
+        <ListContacts 
+          contacts={this.state.contacts} 
+          onDeletContact={this.removeContact}
+        />
       </div>
     );
   }
