0c7e747f13864447111577fb11a326e65dba0ebc
marcio <marciocamello@outlook.com>
2019-01-18 09:37:19 -0200

    Finish lesson 8

--- reactnd-contacts-app/src/App.js
+++ reactnd-contacts-app/src/App.js
@@ -1,32 +1,34 @@
 import React, { Component } from 'react'
 import ListContacts from './components/Contacts/ListContacts'
 
-const contacts = [
-  {
-    "id": "karen",
-    "name": "Karen Isgrigg",
-    "handle": "karen_isgrigg",
-    "avatarURL": "http://localhost:5001/karen.jpg"
-  },
-  {
-    "id": "richard",
-    "name": "Richard Kalehoff",
-    "handle": "richardkalehoff",
-    "avatarURL": "http://localhost:5001/richard.jpg"
-  },
-  {
-    "id": "tyler",
-    "name": "Tyler McGinnis",
-    "handle": "tylermcginnis",
-    "avatarURL": "http://localhost:5001/tyler.jpg"
-  }
- ];
-
 class App extends Component {
+  state = {
+    contacts: [
+      {
+        "id": "karen",
+        "name": "Karen Isgrigg",
+        "handle": "karen_isgrigg",
+        "avatarURL": "http://localhost:5001/karen.jpg"
+      },
+      {
+        "id": "richard",
+        "name": "Richard Kalehoff",
+        "handle": "richardkalehoff",
+        "avatarURL": "http://localhost:5001/richard.jpg"
+      },
+      {
+        "id": "tyler",
+        "name": "Tyler McGinnis",
+        "handle": "tylermcginnis",
+        "avatarURL": "http://localhost:5001/tyler.jpg"
+      }
+     ]
+  }
+
   render() {
     return (
       <div>
-        <ListContacts contacts={contacts} />
+        <ListContacts contacts={this.state.contacts} />
       </div>
     );
   }
--- reactnd-contacts-app/src/components/Contacts/ListContacts.js
+++ reactnd-contacts-app/src/components/Contacts/ListContacts.js
@@ -1,31 +1,27 @@
-import React, { Component } from 'react'
+import React from 'react'
 
 import './ListContacts.css';
 
-class ListContacts extends Component {
-    render() {
-        return (
-            <ol className='contact-list'>
-                {this.props.contacts.map(contact => (
-                    <li key={contact.id} className='contact-list-item'>
-                        <div 
-                            className='contact-avatar'
-                            style={{
-                                backgroundImage: `url(${contact.avatarURL})`
-                            }}
-                        ></div>
-                        <div className='contact-details'>
-                            <p>{contact.name}</p>
-                            <p>{contact.handle}</p>
-                        </div>
-                        <button className='contact-remove'>
-                            Remove
-                        </button>
-                    </li>
-                ))}
-            </ol>
-        )
-    }
-}
+const ListContacts = props => (
+    <ol className='contact-list'>
+        {props.contacts.map(contact => (
+            <li key={contact.id} className='contact-list-item'>
+                <div 
+                    className='contact-avatar'
+                    style={{
+                        backgroundImage: `url(${contact.avatarURL})`
+                    }}
+                ></div>
+                <div className='contact-details'>
+                    <p>{contact.name}</p>
+                    <p>{contact.handle}</p>
+                </div>
+                <button className='contact-remove'>
+                    Remove
+                </button>
+            </li>
+        ))}
+    </ol>
+);
 
 export default ListContacts;
